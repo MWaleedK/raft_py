b6b712d55de859ff6a410f366a2ce629fcb7b103
MWaleedK <waleedjaved911@gmail.com>
2026-09-02 20:44:12 +0500

    hating the start > len(message) check

--- echo_client.py
+++ echo_client.py
@@ -8,19 +8,21 @@ server_address = ('localhost', 10000)
 print(f"connecting to {server_address[0]}, port: {server_address[1]}")
 sock.connect(server_address)
 
-try:
-    message = input("Your Message")
-    print(f"sending message: {message}")
-    sock.sendall(message.encode('utf-8')) #encode in utf-8 format which is bytes and sockets communicate in bytes
+
+while True:
+    try:
+        message = input("Your Message: \n")
+        print(f"sending message: {message}")
+        sock.sendall(message.encode('utf-8')) #encode in utf-8 format which is bytes and sockets communicate in bytes
 
 
-    amount_received = 0
-    amount_expected = len(message)
+        amount_received = 0
+        amount_expected = len(message)
 
-    while amount_received < amount_expected:
-        data = sock.recv(16)
-        amount_received += len(data)
-        print(f"received: {data}")
-finally:
-    print("Closing Socket")
-    sock.close()
+        while amount_received < amount_expected:
+            data = sock.recv(16)
+            amount_received += len(data)
+            print(f"received: {data}")
+    except:
+        print("Closing Socket")
+        sock.close()
--- echo_server.py
+++ echo_server.py
@@ -1,29 +1,65 @@
 import socket
 import sys
 
-server_address = ('localhost', 10000)
-print(f"Starting server on {server_address[0]}, port: {server_address[1]}")
 
-sock = socket.socket()
 
-sock.bind(server_address) #bind the server to the (ip, socket)
+data = {}
 
-sock.listen(1)# listen for connection(s) from client(s)
+def set(key, value):
+    data[key] = value
 
-while True:
-    print("Waiting for a connection")
-    connection, client_address = sock.accept()
+def get(key):
+    return data[key]
 
-    try:
-        print(f"Connection from Client: {client_address}")
-        while True:
-            data = connection.recv(16) #RECV 16 BUTES AT A TIME
-            print(f"received: {data}")
-            if data:
-                print("Sending back data to client")
-                connection.sendall(data)
-            else:
-                print(f"no more data from {client_address}")
-                break
-    finally:
-        connection.close()
+def delete(key):
+    del data[key]
+
+
+
+
+
+
+
+def run_server():
+    server_address = ('localhost', 10000)
+    print(f"Starting server on {server_address[0]}, port: {server_address[1]}")
+
+    sock = socket.socket()
+
+    sock.bind(server_address) #bind the server to the (ip, socket)
+
+    sock.listen(1)# listen for connection(s) from client(s)
+
+    while True:
+        print("Waiting for a connection")
+        connection, client_address = sock.accept()
+
+        try:
+            print(f"Connection from Client: {client_address}")
+            while True:
+                operation = connection.recv(16) #RECV 16 BUTES AT A TIME
+                string_operation = operation.decode('utf-8')
+                print(f"received: {string_operation} of type: {type(string_operation)}")
+                if operation: #instead of sending data back now, we are performing operations
+                    command, key, value = 0,1,2
+                    operands = string_operation.split(" ")
+                    response = "I don't understand that command"
+                    if operands[command] == "get":
+                        response = get(operands[key])
+                    elif operands[command] == "set":
+                        set(operands[key], operands[value])
+                        response = f"{operands[key]} set to {operands[value]}"
+                    elif operands[command] == "delete":
+                        response = f"key {operands[key]} deleted"
+                        delete(operands[key])
+                    elif operands[command] == "show":
+                        response = str(data)
+                    else:
+                        print("None of the conditions hit")
+                    connection.sendall(response.encode('utf-8'))
+                else:
+                    print(f"no more data from {client_address}")
+                    break
+        finally:
+            connection.close()
+run_server()
